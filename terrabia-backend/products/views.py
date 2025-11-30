caa883b4647cfabe34cf3c095bc3c39e1464fa4a
maamoc ronel <ronelmaamoc52@gmail.com>
2025-11-30 16:43:35 +0100

    modif

--- terrabia-backend/products/views.py
+++ terrabia-backend/products/views.py
@@ -1,25 +1,67 @@
 # products/views.py
 from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, BasePermission
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
-# Custom permission: seul le farmer peut modifier ses produits
-from rest_framework.permissions import BasePermission
-
+# Permission personnalisée : seul le farmer peut modifier ses produits
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
         return obj.farmer == request.user
 
-# Catégories
-class CategoryListView(generics.ListAPIView):
+# Permission personnalisée pour les catégories
+class CanCreateCategoryOrReadOnly(BasePermission):
+    """
+    Permission personnalisée :
+    - Lecture : Autorisé à tous
+    - Création : Admins et agriculteurs
+    - Modification/Suppression : Admins seulement
+    """
+    def has_permission(self, request, view):
+        # GET, HEAD, OPTIONS autorisés à tous
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        
+        # POST : seulement admins et agriculteurs
+        if request.method == 'POST':
+            return request.user.is_authenticated and (
+                request.user.is_staff or 
+                request.user.is_superuser or 
+                request.user.user_type == 'farmer'
+            )
+        
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        # GET, HEAD, OPTIONS autorisés à tous
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        
+        # PUT, PATCH, DELETE : seulement admins
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return request.user.is_authenticated and (
+                request.user.is_staff or 
+                request.user.is_superuser
+            )
+        
+        return False
+
+# Catégories - MODIFIÉ
+class CategoryListCreateView(generics.ListCreateAPIView):  # Changé de ListAPIView à ListCreateAPIView
     queryset = Category.objects.filter(parent=None)
     serializer_class = CategorySerializer
+    permission_classes = [CanCreateCategoryOrReadOnly]  # Nouvelle permission
 
-# Produits
+# Vue pour modifier/supprimer les catégories (admin seulement)
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [CanCreateCategoryOrReadOnly]
+
+# Produits (inchangé)
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.filter(available=True)
     serializer_class = ProductSerializer
@@ -31,7 +73,6 @@ class ProductListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(farmer=self.request.user)
-
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
